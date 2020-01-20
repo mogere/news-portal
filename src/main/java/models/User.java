54b1629f6ec16b9896e387ec683bfda1d53d21bb
mogere <pmnyatichi@gmail.com>
2020-01-20 10:14:24 +0300

    add user class code and test file

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,64 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
+    private int userId;
+    private String username;
+    private String position;
+    private String role;
+
+    public User(String username, String position, String role) {
+        this.username = username;
+        this.position = position;
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return userId == user.userId &&
+                Objects.equals(username, user.username) &&
+                Objects.equals(position, user.position) &&
+                Objects.equals(role, user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username, position, role);
+    }
 }
