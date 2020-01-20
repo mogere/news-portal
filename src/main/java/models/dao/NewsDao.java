6411b3f1840a929e917cfe120abeff150ead2969
mogere <pmnyatichi@gmail.com>
2020-01-20 12:35:04 +0300

    add methods to newsDao interface

--- src/main/java/models/dao/NewsDao.java
+++ src/main/java/models/dao/NewsDao.java
@@ -1,4 +1,17 @@
 package models.dao;
 
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    //add news item to db
+    void addNews(News news);
+
+    //return list of general news items
+    List<News> getGeneralNews();
+
+    //return departmental news
+    List<News> getDepartmentNews(int departmentId);
 }
--- src/main/java/models/dao/UserDao.java
+++ src/main/java/models/dao/UserDao.java
@@ -7,9 +7,16 @@ import java.util.List;
 
 public interface UserDao {
 
+    //add user to DB
     void add(User user);
+
+    //return list of all users
     List<User> getAll();
+
+    //return list of all users in a department
     List<User> getAllUsersInDepartment(int departmentId);
+
+    //return specific information about a user
     User getUserInfo(int userId);
 
 }
