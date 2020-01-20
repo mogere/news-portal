b8922cc97df50b334dd2a336456adcc0539c419f
mogere <pmnyatichi@gmail.com>
2020-01-20 09:47:05 +0300

    add database files

---
+++ create.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE news_portal;
+CREATE TABLE news (id SERIAL PRIMARY KEY, headline VARCHAR, content VARCHAR, department_id INTEGER);
+CREATE TABLE departments (id SERIAL PRIMARY KEY, name varchar, description VARCHAR);
+CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR, position varchar, role VARCHAR, department_id INTEGER);
+
+CREATE DATABASE news_portal_test WITH TEMPLATE news-portal;
