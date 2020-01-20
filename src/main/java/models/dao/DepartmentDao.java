484ebdd23af5b76d1ce4658ecfc11cfd5038e6d3
mogere <pmnyatichi@gmail.com>
2020-01-20 12:39:34 +0300

    add methods to departmentDao interface

--- src/main/java/models/dao/DepartmentDao.java
+++ src/main/java/models/dao/DepartmentDao.java
@@ -1,4 +1,14 @@
 package models.dao;
 
+import models.Department;
+
+import java.util.List;
+
 public interface DepartmentDao {
+
+    //add department to db
+    void addDepartment(Department department);
+
+    //Return all departments
+    List<Department> allDepartments();
 }
