152f45f8019de7cdfdbe2aac67509258c337bfb0
mogere <pmnyatichi@gmail.com>
2020-01-20 10:07:53 +0300

    add department class code and test file

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,53 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private int departmentId;
+    private String departmentName;
+    private String description;
+
+    public Department(String departmentName, String description){
+        this.departmentName = departmentName;
+        this.description = description;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return departmentId == that.departmentId &&
+                Objects.equals(departmentName, that.departmentName) &&
+                Objects.equals(description, that.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(departmentId, departmentName, description);
+    }
 }
