2085448e693b0434db1038be8f1fe4aea9aebbfb
mogere <pmnyatichi@gmail.com>
2020-01-20 12:17:33 +0300

    add methods to userDao interface

--- src/main/java/models/dao/UserDao.java
+++ src/main/java/models/dao/UserDao.java
@@ -1,4 +1,15 @@
 package models.dao;
 
+
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+
+    void add(User user);
+    List<User> getAll();
+    List<User> getAllUsersInDepartment(int departmentId);
+    User getUserInfo(int userId);
+
 }
